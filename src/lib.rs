4687901e19f34cc88e5fc56ba3931a010ea5c890
DEADBLACKCLOVER <deadblackclover@protonmail.com>
2020-07-29 13:59:17 +0700

    Added doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+//! Library CipherDogs logo for the operating system
+//!
+//! ![cipherdogs](screenshot.png)
+//!
+//! ## Usage
+//! ```rust
+//! use vga::colors::Color16;
+//! use vga::writers::{Graphics640x480x16, GraphicsWriter};
+//! use vga_figures::figures2d::Figures2D;
+//!
+//! use cipherdogs_logo_lib;
+//!
+//! let mode = Graphics640x480x16::new();
+//! mode.set_mode();
+//! mode.clear_screen(Color16::Black);
+//!
+//! let figures = Figures2D::new(mode);
+//!
+//! cipherdogs_logo_lib::draw(figures, Color16::White);
+//! ```
 #![no_std]
 
 use vga::colors::Color16;
